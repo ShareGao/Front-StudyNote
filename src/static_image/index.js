14c4bbd46c160158835a8012f047a4073ef68d7e
gaoge <2431408640@qq.com>
2020-08-18 21:10:01 +0800

    打包静态资源-图片篇

---
+++ src/static_image/index.js
@@ -0,0 +1,12 @@
+import Header from "./header.js";
+import Sidebar from "./sidebar.js";
+import Content from "./content.js";
+import avatar from './1.jpg'
+
+var dom = document.getElementById('root');
+
+console.log(avatar)
+
+new Header();
+new Sidebar();
+new Content();
